c3b1e994c5cc03b09d1d76a983a79a6b7d8ffa26
Ashit Darurmath <ashitpersistent@gmail.com>
2023-02-22 02:03:05 +0530

    add idle run run backward walk backward animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flame/components.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/palette.dart';
+import 'package:flame/sprite.dart';
 import 'package:flame_texturepacker/flame_texturepacker.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
@@ -19,6 +20,10 @@ class mySprite extends FlameGame with HasDraggables {
 
   late SpriteAnimationComponent boyWalk;
   late final JoystickComponent joystick;
+  late SpriteAnimation idle;
+  late SpriteAnimation walkforward;
+  late SpriteAnimation runBoyrun;
+  bool boyFlipped = false;  
 
 
   Future<void> onLoad() async {
@@ -26,10 +31,14 @@ class mySprite extends FlameGame with HasDraggables {
     print('load assets');
 
     final spriteSheet = await fromJSONAtlas('spriteAnimation.png', 'spriteAnimation.json');
-    SpriteAnimation walkforward = SpriteAnimation.spriteList(spriteSheet, stepTime: .07);
+    final spriteSheetIdle = await fromJSONAtlas('idle.png', 'idle.json');
+    final spriteSheetRun = await fromJSONAtlas('run.png', 'run.json');
+    walkforward = SpriteAnimation.spriteList(spriteSheet, stepTime: .07);
+    idle = SpriteAnimation.spriteList(spriteSheetIdle, stepTime: .07);
+    runBoyrun = SpriteAnimation.spriteList(spriteSheetRun, stepTime: 0.04);
     boyWalk = SpriteAnimationComponent()
     ..animation = walkforward
-    ..position = Vector2(50, 150)
+    ..position = Vector2(50, 200)
     ..size = Vector2(100,100)
     ..anchor = Anchor.center;
     add(boyWalk);
@@ -38,18 +47,46 @@ class mySprite extends FlameGame with HasDraggables {
     final backgroundPaint = BasicPalette.blue.withAlpha(100).paint();
     joystick = JoystickComponent(
     knob: CircleComponent(radius: 30, paint: knobPaint),
-    background: CircleComponent (radius: 100, paint: backgroundPaint),
-    margin: const EdgeInsets.only (left: 140, bottom: 40),
+    background: CircleComponent (radius: 50, paint: backgroundPaint),
+    margin: const EdgeInsets.only (left: 680, bottom: 40),
     );
     add(joystick);
     
   }
 
+  @override
   void update(double dt){
     super.update(dt);
     final Vector2 xpos = joystick.relativeDelta;
     xpos[1] =0;
-    boyWalk.position.add(xpos * 300 *dt); //dt = 1/60 its delta time
+    if( xpos[0]<0 && !boyFlipped ) {
+      boyFlipped = true;
+      boyWalk.flipHorizontallyAroundCenter();
+      boyWalk.animation = walkforward;
+    }
+    else if ( xpos[0]>0 && boyFlipped ) {
+      boyFlipped = false;
+      boyWalk.flipHorizontallyAroundCenter();
+      boyWalk.animation = walkforward;
+    }
+    if ( xpos[0]==0 ) {
+      boyWalk.animation = idle;
+    }
+    else { 
+      boyWalk.animation = walkforward;
+    }
+
+    if(xpos[0]>0.8 || xpos[0]<-0.8){
+      boyWalk.animation = runBoyrun;
+      boyWalk.position.add(xpos * 150 *dt);
+    }
+    else{
+      boyWalk.position.add(xpos * 80 *dt);
+    }
+
+    
+    print(xpos[0]);
+     //dt = 1/60 its delta time
   }
 
 }
