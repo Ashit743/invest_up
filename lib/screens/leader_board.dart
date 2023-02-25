6b8a71efd11502dde2cb92988285f4182679b4c4
Saurav Kumar <saurav0001kumar@gmail.com>
2023-02-25 23:42:39 +0530

    Debug banner removed from leaderboard

--- lib/screens/leader_board.dart
+++ lib/screens/leader_board.dart
@@ -38,6 +38,7 @@ class _MyAppState extends State<LeaderBoard> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         bottomNavigationBar: BottomAppBar(
           child: Container(
